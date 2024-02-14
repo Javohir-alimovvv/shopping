15e89ed15a513dff1c90ac2dab21d1e34250584d
javohir <ajavohir969@gmail.com>
2024-02-14 09:40:04 +0500

    shopping

---
+++ assets/js/app.js
@@ -0,0 +1,145 @@
+
+
+const bgEl = document.querySelector(".card_1");
+const textEl = document.querySelector(".text");
+const btnEl = document.querySelector(".btn");
+const text_twoEl = document.querySelector(".text_two")
+
+btnEl.addEventListener('click', () => {
+    bgEl.style.backgroundColor = "grey";
+    textEl.style.color = "gold"
+    btnEl.style.backgroundColor = "blue"
+    text_twoEl.style.color = "gold"
+});
+
+const cardEl = document.querySelector(".card_2");
+const text_2El = document.querySelector(".text_2");
+const btn_2El = document.querySelector(".btn_2");
+const text_two_2El = document.querySelector(".text_two_2")
+
+btn_2El.addEventListener('click', () => {
+    cardEl.style.backgroundColor = "grey";
+    text_2El.style.color = "gold"
+    btn_2El.style.backgroundColor = "blue"
+    text_two_2El.style.color = "gold"
+});
+
+const card_3El = document.querySelector(".card_3");
+const text_3El = document.querySelector(".text_3");
+const btn_3El = document.querySelector(".btn_3");
+const text_two_3El = document.querySelector(".text_two_3")
+
+btn_3El.addEventListener('click', () => {
+    card_3El.style.backgroundColor = "grey";
+    text_3El.style.color = "gold"
+    btn_3El.style.backgroundColor = "blue"
+    text_two_3El.style.color = "gold"
+});
+
+const card_4El = document.querySelector(".card_4");
+const text_4El = document.querySelector(".text_4");
+const btn_4El = document.querySelector(".btn_4");
+const text_two_4El = document.querySelector(".text_two_4")
+
+btn_4El.addEventListener('click', () => {
+    card_4El.style.backgroundColor = "grey";
+    text_4El.style.color = "gold"
+    btn_4El.style.backgroundColor = "blue"
+    text_two_4El.style.color = "gold"
+});
+
+const card_5El = document.querySelector(".card_5");
+const text_5El = document.querySelector(".text_5");
+const btn_5El = document.querySelector(".btn_5");
+const text_two_5El = document.querySelector(".text_two_5")
+
+btn_5El.addEventListener('click', () => {
+    card_5El.style.backgroundColor = "grey";
+    text_5El.style.color = "gold"
+    btn_5El.style.backgroundColor = "blue"
+    text_two_5El.style.color = "gold"
+});
+
+const card_6El = document.querySelector(".card_6");
+const text_6El = document.querySelector(".text_6");
+const btn_6El = document.querySelector(".btn_6");
+const text_two_6El = document.querySelector(".text_two_6")
+
+btn_6El.addEventListener('click', () => {
+    card_6El.style.backgroundColor = "grey";
+    text_6El.style.color = "gold"
+    btn_6El.style.backgroundColor = "blue"
+    text_two_6El.style.color = "gold"
+});
+
+const card_7El = document.querySelector(".card_7");
+const text_7El = document.querySelector(".text_7");
+const btn_7El = document.querySelector(".btn_7");
+const text_two_7El = document.querySelector(".text_two_7")
+
+btn_7El.addEventListener('click', () => {
+    card_7El.style.backgroundColor = "grey";
+    text_7El.style.color = "gold"
+    btn_7El.style.backgroundColor = "blue"
+    text_two_7El.style.color = "gold"
+});
+
+const card_8El = document.querySelector(".card_8");
+const text_8El = document.querySelector(".text_8");
+const btn_8El = document.querySelector(".btn_8");
+const text_two_8El = document.querySelector(".text_two_8")
+
+btn_8El.addEventListener('click', () => {
+    card_8El.style.backgroundColor = "grey";
+    text_8El.style.color = "gold"
+    btn_8El.style.backgroundColor = "blue"
+    text_two_8El.style.color = "gold"
+});
+
+const card_9El = document.querySelector(".card_9");
+const text_9El = document.querySelector(".text_9");
+const btn_9El = document.querySelector(".btn_9");
+const text_two_9El = document.querySelector(".text_two_9")
+
+btn_9El.addEventListener('click', () => {
+    card_9El.style.backgroundColor = "grey";
+    text_9El.style.color = "gold"
+    btn_9El.style.backgroundColor = "blue"
+    text_two_9El.style.color = "gold"
+});
+
+const card_10El = document.querySelector(".card_10");
+const text_10El = document.querySelector(".text_10");
+const btn_10El = document.querySelector(".btn_10");
+const text_two_10El = document.querySelector(".text_two_10")
+
+btn_10El.addEventListener('click', () => {
+    card_10El.style.backgroundColor = "grey";
+    text_10El.style.color = "gold"
+    btn_10El.style.backgroundColor = "blue"
+    text_two_10El.style.color = "gold"
+});
+
+const card_11El = document.querySelector(".card_11");
+const text_11El = document.querySelector(".text_11");
+const btn_11El = document.querySelector(".btn_11");
+const text_two_11El = document.querySelector(".text_two_11")
+
+btn_11El.addEventListener('click', () => {
+    card_11El.style.backgroundColor = "grey";
+    text_11El.style.color = "gold"
+    btn_11El.style.backgroundColor = "blue"
+    text_two_11El.style.color = "gold"
+});
+
+const card_12El = document.querySelector(".card_12");
+const text_12El = document.querySelector(".text_12");
+const btn_12El = document.querySelector(".btn_12");
+const text_two_12El = document.querySelector(".text_two_12")
+
+btn_12El.addEventListener('click', () => {
+    card_12El.style.backgroundColor = "grey";
+    text_12El.style.color = "gold"
+    btn_12El.style.backgroundColor = "blue"
+    text_two_12El.style.color = "gold"
+});
